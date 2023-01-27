61bb3499d62226fe31dddf16e2676a299facbe20
LeiZhe <1002902525@qq.com>
2023-01-27 21:36:35 +0800

    add UNet

--- datasets.py
+++ datasets.py
@@ -12,23 +12,24 @@ from utils import QRcode
 
 
 class trainDataset(Dataset):
-    def __init__(self, path):
+    def __init__(self, path, cache_path):
         self.path = path
+        self.cache_path = cache_path
         self.all_train_data_path = sorted(glob.glob(os.path.join(path, 'train/*.jpg')))
 
     def __len__(self):
         return len(self.all_train_data_path)
 
     def __getitem__(self, idx):
-        img_t = T.Resize(size=(256, 256))(read_image(self.all_train_data_path[idx]))
+        img_t = T.Resize(size=(256, 256))(read_image(self.all_train_data_path[idx]).to(torch.float32))
         i, j, h, w = T.RandomCrop.get_params(img_t, (96, 96))
         img_cropped_t = crop(img_t, i, j, h, w)
 
         message = ''.join(str(i) for i in random.choices([0, 1], k=96))
-        qrcode = QRcode.QRcode().message2img(message)
+        qrcode = QRcode.QRcode(self.cache_path).message2img(message)
 
         input_encoder = torch.cat((img_cropped_t, qrcode), 0)
-        print(input_encoder.shape)
+        # print(input_encoder.shape)
         return img_t, input_encoder, (i, j)
 
 
@@ -49,9 +50,9 @@ class validateDataset(Dataset):
 
 if __name__ == "__main__":
     img_t, img_cropped_t, location = trainDataset('../data/LIMHC').__getitem__(2)
-    # print(img_t.shape)
-    # print(img_cropped_t.shape)
-    # print(location)
+    print(img_t.shape)
+    print(img_cropped_t.shape)
+    print(location)
     # img = T.ToPILImage()(img_cropped_t)
     # img.show()
     # img = T.ToPILImage()(img_t)
--- utils/QRcode.py
+++ utils/QRcode.py
@@ -9,7 +9,8 @@ from torchvision.io import read_image, ImageReadMode
 
 
 class QRcode:
-    def __init__(self):
+    def __init__(self, cache_path):
+        self.cache_path = cache_path
         self.qr = qrcode.QRCode(
             version=None,
             error_correction=qrcode.constants.ERROR_CORRECT_H,
@@ -23,16 +24,16 @@ class QRcode:
         self.qr.clear()
         self.qr.add_data(message)
         self.qr.make(fit=True)
-        self.qr.make_image(fill_color="black", back_color="white").save('.cache/' + file_name)
-        ret = T.Resize(size=(96, 96))(read_image(os.path.join('.cache/' + file_name), ImageReadMode.GRAY))
-        os.remove(os.path.join('.cache/' + file_name))
+        self.qr.make_image(fill_color="black", back_color="white").save(os.path.join(self.cache_path, file_name))
+        ret = T.Resize(size=(96, 96))(read_image(os.path.join(self.cache_path, file_name), ImageReadMode.GRAY))
+        os.remove(os.path.join(self.cache_path, file_name))
         return ret
 
     def img2message(self, img):
         file_name = str(random.randint(1, 10000)) + '.png'
-        F.to_pil_image(img).save('.cache/' + file_name)
-        results = self.reader.decode('.cache/' + file_name)
-        os.remove(os.path.join('.cache/' + file_name))
+        F.to_pil_image(img).save(os.path.join(self.cache_path, file_name))
+        results = self.reader.decode(os.path.join(self.cache_path, file_name))
+        os.remove(os.path.join(self.cache_path, file_name))
         print(results[0]['parsed'])
 
 
@@ -40,5 +41,5 @@ if __name__ == "__main__":
     # x = QRcode().message2img('111')
     # QRcode().img2message(x)
     message = ''.join(str(i) for i in random.choices([0, 1], k=96))
-    x = QRcode().message2img(message)
-    QRcode().img2message(x)
+    x = QRcode('../.cache').message2img(message)
+    QRcode('../.cache').img2message(x)
