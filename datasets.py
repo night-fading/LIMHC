885e24c16cd4376de30a5d8e0cbea77ce709ec10
LeiZhe <1002902525@qq.com>
2023-01-29 21:14:10 +0800

    add distortion

--- datasets.py
+++ datasets.py
@@ -2,10 +2,9 @@ import glob
 import os.path
 import random
 
-import matplotlib
-import matplotlib.pyplot as plt
 import torch
 import torchvision.transforms as T
+from matplotlib import pyplot as plt
 from torch.utils.data import Dataset
 from torchvision.io import read_image
 from torchvision.transforms.functional import crop
@@ -23,7 +22,8 @@ class trainDataset(Dataset):
         return len(self.all_train_data_path)
 
     def __getitem__(self, idx):
-        img_t = T.Resize(size=(256, 256))(read_image(self.all_train_data_path[idx]).to(torch.float32))
+        img_t = T.Resize(size=(256, 256))(read_image(self.all_train_data_path[idx]).to(torch.float32) / 255)
+
         i, j, h, w = T.RandomCrop.get_params(img_t, (96, 96))
         img_cropped_t = crop(img_t, i, j, h, w)
 
@@ -32,31 +32,31 @@ class trainDataset(Dataset):
 
         input_encoder = torch.cat((img_cropped_t, qrcode), 0)
         # print(input_encoder.shape)
-        return img_t, input_encoder, (i, j)
+        return img_t, input_encoder, [(i, j), (i, j + 96), (i + 96, j + 96), (i + 96, j)]
 
 
-class validateDataset(Dataset):
-    def __init__(self, path):
-        self.path = path
-        self.all_val_data_path = sorted(glob.glob(os.path.join(path, 'val/*.jpg')))
-
-    def __len__(self):
-        return len(self.all_val_data_path)
-
-    def __getitem__(self, idx):
-        img_t = T.Resize(size=(256, 256))(read_image(self.all_val_data_path[idx]))
-        i, j, h, w = T.RandomCrop.get_params(img_t, (96, 96))
-        img_cropped_t = crop(img_t, i, j, h, w)
-        return img_t, img_cropped_t, (i, j)
+# class validateDataset(Dataset):
+#     def __init__(self, path):
+#         self.path = path
+#         self.all_val_data_path = sorted(glob.glob(os.path.join(path, 'val/*.jpg')))
+#
+#     def __len__(self):
+#         return len(self.all_val_data_path)
+#
+#     def __getitem__(self, idx):
+#         img_t = T.Resize(size=(256, 256))(read_image(self.all_val_data_path[idx]))
+#         i, j, h, w = T.RandomCrop.get_params(img_t, (96, 96))
+#         img_cropped_t = crop(img_t, i, j, h, w)
+#         return img_t, img_cropped_t, (i, j)
 
 
 if __name__ == "__main__":
-    print(matplotlib.get_backend())
     img_t, img_cropped_t, location = trainDataset('../data/LIMHC', '.cache').__getitem__(0)
     print(img_cropped_t.shape)
     print(location)
-    print(img_t.to(torch.uint8).dtype)
-    plt.imshow(img_t.to(torch.uint8).permute(1, 2, 0))
+    # print(img_t)
+    print((img_t > 1).any())
+    plt.imshow(img_t.permute(1, 2, 0))
     plt.show()
     # img = T.ToPILImage()(img_cropped_t)
     # img.show()
--- utils/transforms.py
+++ utils/transforms.py
@@ -1,4 +1,8 @@
+import cv2 as cv
+import numpy as np
 import torch
+import torchvision.transforms as T
+import torchvision.transforms.functional as F
 from matplotlib import pyplot as plt
 
 from datasets import trainDataset
@@ -6,23 +10,64 @@ from model.unet import UNet
 
 
 def replaceImage(replaced_image, image, position):
-    print(replaced_image.shape)
-
-    i, j = position
+    i, j = position[0]
     replaced_image[:, i: i + 96, j:j + 96] = image
-    print(replaced_image.shape)
-    plt.imshow(replaced_image.to(torch.uint8).permute(1, 2, 0))
+    return replaced_image
+
+
+def distort(img, position):
+    # print(img)
+    img_d = img.detach()
+    plt.imshow(img_d.permute(1, 2, 0))
     plt.show()
+
+    img = T.ColorJitter(.5, .5, .5, .1)(img)
+    img = T.GaussianBlur(3, (0.1, 1))(img)
+    img, pos = perspectiveTransform(img, position)
+
+    return img, pos
+
+
+def perspectiveTransform(img, position):
+    pts1, pts2 = T.RandomPerspective().get_params(256, 256, 0.5)
+    img = F.perspective(img, pts1, pts2)
+
+    pts1 = np.float32(pts1)
+    pts2 = np.float32(pts2)
+    M = cv.getPerspectiveTransform(pts1, pts2)
+
+    position[0] = cvt_pos((position[0][1], position[0][0]), M)
+    position[1] = cvt_pos((position[1][1], position[1][0]), M)
+    position[2] = cvt_pos((position[2][1], position[2][0]), M)
+    position[3] = cvt_pos((position[3][1], position[3][0]), M)
+
+    # img = (img * 255).to(torch.uint8)
+    # pos = torch.tensor([[position[3][0], position[3][1], position[3][0]+1, position[3][1]+1]], dtype=torch.float)
+    # img = draw_bounding_boxes(img, pos, colors=["blue"], width=5)
+
+    return img, position
+
+
+def cvt_pos(position, cvt_mat_t):
+    u, v = position
+    x = (cvt_mat_t[0][0] * u + cvt_mat_t[0][1] * v + cvt_mat_t[0][2]) / (
+            cvt_mat_t[2][0] * u + cvt_mat_t[2][1] * v + cvt_mat_t[2][2])
+    y = (cvt_mat_t[1][0] * u + cvt_mat_t[1][1] * v + cvt_mat_t[1][2]) / (
+            cvt_mat_t[2][0] * u + cvt_mat_t[2][1] * v + cvt_mat_t[2][2])
+    return (int(x), int(y))
 
 
 if __name__ == "__main__":
-    img_t, img_cropped_t, location = trainDataset('../../data/LIMHC', '../.cache').__getitem__(4)
+    img_t, img_cropped_t, position = trainDataset('../../data/LIMHC', '../.cache').__getitem__(3)
     net = UNet()
     img_cropped_t = img_cropped_t.unsqueeze(0)
-    # img = T.ToPILImage()(img_t)
-    # img.show()
-    # print(img_cropped_t.shape)
     x = net.forward(img_cropped_t)
-    x = x.squeeze(0)
-
-    replaceImage(img_t, x, position=location)
+    x = torch.clamp(x, min=0.0, max=1.0)
+    # print((x > 1.0).any())
+    x = replaceImage(img_t, x, position=position)
+    x, pos = distort(x, position)
+    # x = perspectiveTransform(x, position)
+    print(type(x))
+    x_d = x.detach()
+    plt.imshow(x_d.permute(1, 2, 0))
+    plt.show()
